bdca0b5a46b5a345287aa15b5ca46e24a012ed26
Chimmy <jimmyeetran@gmail.com>
2023-05-18 23:34:18 +1000

    implemented first schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,20 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import pet from './schemas.ts/pet'
+
+const post = {
+  name: 'post',
+  title: 'Posts',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    }
+  ]
+}
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [post],
 }
+
