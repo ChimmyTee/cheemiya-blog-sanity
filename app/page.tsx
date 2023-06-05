4d0bf7aac24fda6bc399faf7cc2e2677cfdd1dca
Chimmy <jimmyeetran@gmail.com>
2023-06-05 23:54:30 +1000

    added the insert image feature to the PortableTextEditor in studio

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,31 @@
+'use client';
 import Image from 'next/image'
 import { getFoodReviews } from '@/sanity/lib/sanity-utils'
+import food from '@/sanity/schemas/foodReview';
+import { PortableText } from '@portabletext/react'
+// import { PortableTextTypeComponent, PortableTextTypeComponentProps } from '@portabletext/react';
+
 
 export default async function Home() {
   const foodReviews = await getFoodReviews();
+
   return (
     <div className='max-w-5xl mx-auto'>
+
       {
         foodReviews.map((foodReview) => (
-          <div key={foodReview._id}>{foodReview.name}{foodReview.image}</div>
+          <div key={foodReview._id}>{foodReview.name}{foodReview.image}
+            <div className='text-lg text-gray-70 mt-10'>
+              <PortableText
+                value={foodReview.content}
+                components={{
+                  types: {
+                    image: ({value}) => <Image src={value.imageUrl} alt={'fdsa'} />,
+                  },
+                }}
+              />
+            </div>
+          </div>
         ))
       }
       food reviews are here
--- sanity/schemas/foodReview.ts
+++ sanity/schemas/foodReview.ts
@@ -36,7 +36,7 @@ const food = {
         name: 'content',
         title: 'Content',
         type: 'array',
-        of: [{ type: "block" }]
+        of: [{ type: "block" },  { type: 'image'}]
     }
   ],
 };
